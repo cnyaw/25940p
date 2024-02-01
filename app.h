ba9f5fb95e1df253848bff6970945b2c3613feb2
cnyaw <wayncc@gmail.com>
2024-02-01 15:21:56 +0800

    Loaded texture in common way.

--- app.h
+++ app.h
@@ -147,11 +147,7 @@ public:
       texture = 0;
       glGenTextures(1, &texture);
       glBindTexture(GL_TEXTURE_2D, texture);
-#ifdef __EMSCRIPTEN__
       glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, img.w, img.h, 0, GL_RGBA, GL_UNSIGNED_BYTE, img.dat);
-#else
-      glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, img.w, img.h, 0, GL_BGRA_EXT, GL_UNSIGNED_BYTE, img.dat);
-#endif
       glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
       glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     }
